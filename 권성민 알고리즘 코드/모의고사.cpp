2c8fdebbc4aab4ecc55a9c0a526eccf6a2651599
kwonja <snna58@naver.com>
2021-11-09 17:32:09 +0900

    [PG]모의고사-Lv.1

---
+++ 권성민 알고리즘 코드/모의고사.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+	vector<int> winner;
+	vector<int> firstarray = { 1,2,3,4,5 };
+	vector<int> secondarray = { 2,1,2,3,2,4,2,5 };
+	vector<int> thirdarray = { 3,3,1,1,2,2,4,4,5,5 };
+	int firstcount = 0; int secondcount = 0; int thirdcount = 0;
+	for (int i = 0; i < answers.size(); i++)
+	{
+		if (answers[i] == firstarray[i % 5])
+			firstcount++;
+		if (answers[i] == secondarray[i % 8])
+			secondcount++;
+		if (answers[i] == thirdarray[i % 10])
+			thirdcount++;
+	}
+	if (firstcount >= secondcount && firstcount >= thirdcount)
+	{
+		winner.push_back(1);
+	}
+	if (secondcount >= firstcount && secondcount >= thirdcount)
+	{
+		winner.push_back(2);
+	}
+	if (thirdcount >= firstcount && thirdcount >= secondcount)
+	{
+		winner.push_back(3);
+	}
+	return winner;
+}
